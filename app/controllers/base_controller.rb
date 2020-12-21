1c8b5aedf585700f9a4825ee23bf7e7de8653f00
tolves <tov.sharing@gmail.com>
2020-12-21 13:24:02 +0000

    i18n settings

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -10,6 +10,16 @@ class BaseController < Telegram::Bot::UpdatesController
   end
 
   def send_message(text)
+    set_locale
     respond_with :message, text: text
   end
+
+  def set_locale
+    I18n.locale = locale_code
+  end
+
+  def locale_code
+    from['language_code'][0..1]
+  end
+
 end
