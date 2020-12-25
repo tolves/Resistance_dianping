e307fc20b3304dcbb444fa91d713dbc517baad25
tolves <tov.sharing@gmail.com>
2020-12-25 23:21:17 +0000

    test configuration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  Telegram.reset_bots
+
+  Telegram::Bot::ClientStub.stub_all!
+
 end
