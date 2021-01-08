5dc903efecd6bd8683aa21f39a06d10e71dfa1bc
tolves <tov.sharing@gmail.com>
2021-01-08 16:29:51 +0000

    optimise: cities keyboard

--- app/controllers/concerns/cities.rb
+++ app/controllers/concerns/cities.rb
@@ -11,7 +11,7 @@ module Cities
     page = args[:page].to_i
     Rails.cache.fetch(":cities/#{changed.size}/#{page}") do
       puts 'update caches'
-      cities = Restaurant.select(:city_id).group(:city_id).limit(@pg_offset * @group_size).offset(page * @pg_offset * @group_size).map { |c| { text: c.city.name, callback_data: "list_restaurants:#{c.city_id}" } }
+      cities = changed.limit(@pg_offset * @group_size).offset(page * @pg_offset * @group_size).map { |c| { text: c.name, callback_data: "list_restaurants:#{c.id}" } }
       cities = cities.in_groups_of(@group_size, false)
       pg_size = (changed.size.to_f / @group_size).ceil
       cities.push pagination(pg_size, @pg_offset, page: page, action: 'edit_cities')
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
   has_many :restaurants
   has_many :statistics
-  scope :changed, -> { where('restaurants_count != 0') }
+  scope :changed, -> { where('restaurants_count != 0').order(updated_at: :desc) }
 end
